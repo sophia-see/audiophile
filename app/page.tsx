c9f2181dd8c10c57914131c4e34b6c76d95d3068
Sophia See <sophiasee11@gmail.com>
2025-03-06 13:13:21 +0800

    added radio group

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 "use client"
 
-import TextField from "@/components/TextField";
+import { RadioGroup, RadioItem } from "@/components/Radio";
+import React from "react";
 
 export default function Home() {
+  const [selected, setSelected] = React.useState("option1");
+
   return (
-    <div className="p-10 flex gap-10">
-      <TextField
-        label="Name"
-        placeholder="Insert your name"
-        error={"Wrong format"}
-      />
+    <div className="p-6">
+      <h2 className="text-lg font-semibold mb-4">Select an Option:</h2>
+      <RadioGroup value={selected} onChange={setSelected}>
+        <RadioItem value="option1">Cash</RadioItem>
+        <RadioItem value="option2">e-money</RadioItem>
+        <RadioItem value="option3">Option 3</RadioItem>
+      </RadioGroup>
+      <p className="mt-4">Selected: {selected}</p>
     </div>
   );
 }
