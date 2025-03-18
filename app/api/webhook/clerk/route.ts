41dc5a90192505b6adf0601d4d01f0573f881919
Sophia See <sophiasee11@gmail.com>
2025-03-18 14:04:41 +0800

    updated webhook clerk

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,14 +1,14 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
 import { createUser } from '@/lib/actions/user.actions'
+import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Error: Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env')
+    throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env')
   }
 
   // Create new Svix instance with secret
@@ -49,16 +49,21 @@ export async function POST(req: Request) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const eventType = evt.type;
-
+  // const { id } = evt.data
+  const eventType = evt.type
+  
+  console.log({eventType})
+  
   if (eventType === 'user.created') {
-    const { id, email_addresses, first_name, last_name } = evt.data;
+    const { id, email_addresses, first_name, last_name} = evt.data;
 
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
+      // username: username!,
       firstName: first_name!,
       lastName: last_name!,
+      // photo: image_url
     }
     console.log("creating new user")
     const newUser = await createUser(user);
@@ -77,5 +82,28 @@ export async function POST(req: Request) {
     return NextResponse.json({message: "OK", user: newUser})
   }
 
-  return new Response('Webhook received', { status: 200 })
+  // if (eventType === 'user.updated') {
+  //   const {id, image_url, first_name, last_name, username } = evt.data
+
+  //   const user = {
+  //     firstName: first_name!,
+  //     lastName: last_name!,
+  //     username: username!,
+  //     photo: image_url,
+  //   }
+
+  //   const updatedUser = await updateUser(id, user)
+
+  //   return NextResponse.json({ message: 'OK', user: updatedUser })
+  // }
+
+  // if (eventType === 'user.deleted') {
+  //   const { id } = evt.data
+
+  //   const deletedUser = await deleteUser(id!)
+
+  //   return NextResponse.json({ message: 'OK', user: deletedUser })
+  // }
+
+  return new Response('', { status: 200 })
 }
