d606d36fdd7b8f6f4becad054c310fcb6c250712
Sophia See <sophiasee11@gmail.com>
2025-03-20 17:37:59 +0800

    log node env

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,13 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+
+
+export default ({ env }) => {
+  console.log("🚀 NODE_ENV DETECTED:", env("NODE_ENV", "not set"));
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+  };
+};
